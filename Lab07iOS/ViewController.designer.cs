9f1eae92d79d8b58dc251c05322e6af0794bbac9
Tony Simoes <sr.tony@gmail.com>
2017-09-14 13:38:13 +0200

    Buscar Producto View.
    Controles situados.

--- Lab07iOS/ViewController.designer.cs
+++ Lab07iOS/ViewController.designer.cs
@@ -1,17 +1,91 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace Lab07iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtnBuscar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtnValidarActividad { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField IDNumber { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LabelEstadoActividad { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextCategoria { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextExistencia { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextNombre { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TextPrecio { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (BtnBuscar != null) {
+                BtnBuscar.Dispose ();
+                BtnBuscar = null;
+            }
+
+            if (BtnValidarActividad != null) {
+                BtnValidarActividad.Dispose ();
+                BtnValidarActividad = null;
+            }
+
+            if (IDNumber != null) {
+                IDNumber.Dispose ();
+                IDNumber = null;
+            }
+
+            if (LabelEstadoActividad != null) {
+                LabelEstadoActividad.Dispose ();
+                LabelEstadoActividad = null;
+            }
+
+            if (TextCategoria != null) {
+                TextCategoria.Dispose ();
+                TextCategoria = null;
+            }
+
+            if (TextExistencia != null) {
+                TextExistencia.Dispose ();
+                TextExistencia = null;
+            }
+
+            if (TextNombre != null) {
+                TextNombre.Dispose ();
+                TextNombre = null;
+            }
+
+            if (TextPrecio != null) {
+                TextPrecio.Dispose ();
+                TextPrecio = null;
+            }
         }
     }
 }
